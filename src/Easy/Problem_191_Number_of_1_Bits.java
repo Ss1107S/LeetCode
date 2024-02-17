39a3c393b5f741c78a8e6e017241ed5fee2adfea
Ss1107S <kashinasvetlana@rambler.ru>
2024-02-17 22:27:16 +0300

    Problem 191

---
+++ src/Easy/Problem_191_Number_of_1_Bits.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class Problem_191_Number_of_1_Bits {
+    //Language Java
+    //Runtime 0ms
+    //Memory 39.6MB
+    public int hammingWeight(int n) {
+        int result=0;
+        for(int i=0; i<32; i++){
+            result+=((n>>i)&1);
+        }
+        return result;
+    }
+}
