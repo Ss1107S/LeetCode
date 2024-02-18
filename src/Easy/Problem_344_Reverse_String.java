63003d6650f40c3aa9c67d15212185127870a67b
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-02-18 19:37:35 +0300

    Add files via upload
    Problem 344

---
+++ src/Easy/Problem_344_Reverse_String.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class Problem_344_Reverse_String {
+    //Language Java
+    //Runtime 0ms
+    //Memory 50.5MB
+
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+
+        while(start<=end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
