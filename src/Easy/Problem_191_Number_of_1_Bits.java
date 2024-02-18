2abd39c015f3b7bafc4918c79188741bc2110d18
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-02-18 19:57:27 +0300

    Problem 191
    Add Problem 191

--- src/Easy/Problem_191_Number_of_1_Bits.java
+++ src/Easy/Problem_191_Number_of_1_Bits.java
@@ -5,9 +5,10 @@ public class Problem_191_Number_of_1_Bits {
     //Runtime 0ms
     //Memory 39.6MB
     public int hammingWeight(int n) {
-        int result=0;
-        for(int i=0; i<32; i++){
-            result+=((n>>i)&1);
+        int result = 0;
+
+        for(int i = 0; i < 32; i++){
+            result+= ((n >> i) & 1);
         }
         return result;
     }
