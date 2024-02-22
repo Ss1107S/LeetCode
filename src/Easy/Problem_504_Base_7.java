a87d89485c842daa75523854ca689c57f454d0d4
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-02-23 00:04:01 +0300

    Problem 504
    Add Problem 504

---
+++ src/Easy/Problem_504_Base_7.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class Problem_504_Base_7 {
+    //Language Java
+    //Runtime 0ms
+    //Memory 40.1MB
+    public String convertToBase7(int num) {
+        return Integer.toString(num,7);
+    }
+}
