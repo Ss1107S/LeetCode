fdd83377153250d900533cf0185384b93cc6e3c0
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-02-25 00:23:47 +0300

    Problem 2264
    Add Problem 2264

--- src/Easy/Problem_2264_Largest_3_Same_Digit_Number_in_String.java
+++ src/Easy/Problem_2264_Largest_3_Same_Digit_Number_in_String.java
@@ -6,11 +6,13 @@ public class Problem_2264_Largest_3_Same_Digit_Number_in_String {
     //Memory 42.6MB
     public String largestGoodInteger(String num) {
         String result = "";
+
         for(int i = 2; i < num.length(); i++)
             if(num.charAt(i) == num.charAt(i-1) &&
                     num.charAt(i - 1) == num.charAt(i - 2))
                 if(num.substring(i - 2, i + 1).compareTo(result) > 0)
                     result = num.substring(i - 2, i + 1);
+        
         return result;
     }
 }
