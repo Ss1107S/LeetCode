d38d108643814b681aee22f3e44ae336f2ce4ce8
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-02-27 00:01:59 +0300

    Problem 258
    Add Problem 258

---
+++ src/Easy/Problem_258_Add_Digits.java
@@ -0,0 +1,12 @@
+package Easy;
+
+public class Problem_258_Add_Digits {
+    //Language Java
+    //Runtime 0ms
+    //Memory 41.2MB
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
+    }
+}
