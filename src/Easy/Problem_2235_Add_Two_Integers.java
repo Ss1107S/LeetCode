bce7c4719de035fdbba96a48879724611adb031e
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-02 00:18:23 +0300

    Problem 2235
    Add Problem 2235

---
+++ src/Easy/Problem_2235_Add_Two_Integers.java
@@ -0,0 +1,11 @@
+package Easy;
+
+public class Problem_2235_Add_Two_Integers {
+    //Language Java
+    //Runtime 1ms
+    //Memory 40.8MB
+    public int sum(int num1, int num2) {
+        int num3 = Integer.sum(num1, num2);
+        return num3;
+    }
+}
