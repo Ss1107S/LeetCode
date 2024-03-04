19ad489932de19267d9b4d8bb7d47e2a45022aed
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-04 11:09:37 +0300

    Problem 1108
    Add Problem 1108

---
+++ src/Easy/Problem_1108_Defanging_and_IP_Address.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class Problem_1108_Defanging_and_IP_Address {
+    //Language Java
+    //Runtime 0ms
+    //Memory 41.3MB
+    public String defangIPaddr(String address) {
+        address = address.replace(".", "[.]"); return address;
+    }
+}
