681bde2ad5e9fea25925a9ad082964d3ee1b3e6a
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-04 11:10:46 +0300

    Problem 709
    Add Problem 709

---
+++ src/Easy/Problem_709_To_Lower_Case.java
@@ -0,0 +1,11 @@
+package Easy;
+
+public class Problem_709_To_Lower_Case {
+    //Language Java
+    //Runtime 0ms
+    //Memory 41.7MB
+    public String toLowerCase(String s) {
+        s = s.toLowerCase();
+        return s;
+    }
+}
