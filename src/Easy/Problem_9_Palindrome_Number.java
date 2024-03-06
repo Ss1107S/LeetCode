47ca1971f0279fb76e71b0a14218a9278d11a158
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-06 13:39:45 +0300

    Problem 9
    Add Problem 9

---
+++ src/Easy/Problem_9_Palindrome_Number.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class Problem_9_Palindrome_Number {
+    //Language Java
+    //Runtime 4ms
+    //Memory 43.9MB
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+
+        int y = 0;
+
+        while (x > y) {
+            y = y * 10 + x % 10;
+            x = x/10;
+        }
+        return (x == y || x == y/10);
+    }
+}
