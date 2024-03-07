722684c256696d485af1039f8328facbf996ea80
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-07 13:10:59 +0300

    Problem 415
    Improved Problem 415

--- src/Easy/Problem_415_Add_Strings.java
+++ src/Easy/Problem_415_Add_Strings.java
@@ -4,9 +4,20 @@ import java.math.BigInteger;
 
 public class Problem_415_Add_Strings {
     //Language Java
-    //Runtime 20ms
-    //Memory 43.9MB
+    //Runtime 1ms
+    //Memory 42.5MB
     public String addStrings(String num1, String num2) {
-        return ((new BigInteger(num1)).add((new  BigInteger(num2)))).toString();
+        StringBuilder count = new StringBuilder();
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0, sum;
+
+        while (i >= 0 || j >= 0 || carry > 0){
+            int digit1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
+            int digit2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
+
+            sum = digit1 + digit2 + carry;
+            count.append(sum % 10);
+            carry = sum / 10;
+        }
+        return count.reverse().toString();
     }
 }
