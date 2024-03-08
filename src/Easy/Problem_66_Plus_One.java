a22f6c0a74e96aa2b9623dcf9a48ea6d1724984e
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-08 22:49:05 +0300

    Problem 66
    Added Problem 66

---
+++ src/Easy/Problem_66_Plus_One.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class Problem_66_Plus_One {
+    //Language Java
+    //Runtime 0ms
+    //Memory 42MB
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
