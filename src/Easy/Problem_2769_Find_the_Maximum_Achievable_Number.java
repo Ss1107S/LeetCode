2469ece77633062ee5cda29b500164b3670f153d
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-11 00:04:32 +0300

    Problem 2769
    Added Problem 2769

---
+++ src/Easy/Problem_2769_Find_the_Maximum_Achievable_Number.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class Problem_2769_Find_the_Maximum_Achievable_Number {
+    //Language Java
+    //Runtime 1ms
+    //Memory 41.5MB
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
