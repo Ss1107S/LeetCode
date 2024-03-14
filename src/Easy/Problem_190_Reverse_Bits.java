a8da5f75f4a58652ffad282792b28dc2cc28bed9
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-14 12:28:55 +0300

    Problem 190
    Added Problem 190

---
+++ src/Easy/Problem_190_Reverse_Bits.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class Problem_190_Reverse_Bits {
+    //Language Java
+    //Runtime 0ms
+    //Memory 42.2MB
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}
