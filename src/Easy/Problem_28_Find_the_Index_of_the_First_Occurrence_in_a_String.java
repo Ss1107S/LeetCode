065ecb3ece32ebabc147d9712d1e6748a0105d6f
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-03-20 12:52:31 +0300

    Problem 28
    Added Problem 28

---
+++ src/Easy/Problem_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class Problem_28_Find_the_Index_of_the_First_Occurrence_in_a_String {
+    //Language Java
+    //Runtime 0ms
+    //Memory 41.4MB
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
