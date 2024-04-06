29e902e2f98edbc14115e19e79bd937986b1ac3f
Ss1107S <152962158+Ss1107S@users.noreply.github.com>
2024-04-06 22:53:52 +0300

    Problem 326
    Improved Problem 326

--- src/Easy/Problem_326_Power_of_Three.java
+++ src/Easy/Problem_326_Power_of_Three.java
@@ -3,11 +3,10 @@ package Easy;
 public class Problem_326_Power_of_Three {
     //Language Java
     //Runtime 8ms
-    //Memory 42.5MB
+    //Memory 44.2MB
     public boolean isPowerOfThree(int n) {
-        if (n < 1) return false;
         if (n == 1) return true;
-        if (n % 3!= 0) return false;
-        return isPowerOfThree(n/3);
+        if (n % 3 != 0 || n == 0) return false;
+        return isPowerOfThree(n / 3);
     }
 }
